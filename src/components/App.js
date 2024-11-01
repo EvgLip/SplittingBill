1f6ccf0df8798f26be2767e002b8122daf2ebc4a
evgLip <evggenlip@gmail.com>
2024-11-01 20:07:41 +0300

    дал более информативные имена переменным состояния функционал не менял

--- src/components/App.js
+++ src/components/App.js
@@ -5,39 +5,16 @@ import FormAddFriend from "./FormAddFriend";
 import FriendsList from "./FriendsList";
 import FormSplitBill from "./FormSplitBill";
 
-//тестовые данные
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
-
 export default function App ()
 {
   const [friends, setFriends] = useState([]);
-  const [add, setAdd] = useState(false);
+  const [showAddFor, setShowAddFor] = useState(false);
   const [selectedFriend, setSelectedFriend] = useState({});
 
   function onAddFriend (friend = {})
   {
     setFriends(f => [...f, friend]);
-    setAdd(true);
+    setShowAddFor(true);
   }
 
   function onSelectFriend (friendId = null)
@@ -67,12 +44,12 @@ export default function App ()
 
       <article className="sidebar">
         <FriendsList friends={friends} onSelectFriend={onSelectFriend} />
-        {
-          add &&
-          <FormAddFriend onAddFriend={onAddFriend} />
-        }
-        <button className="button" onClick={() => setAdd((add) => !add)} >
-          {add ? 'Закрыть' : 'Добавить друга'}
+        {showAddFor && <FormAddFriend onAddFriend={onAddFriend} />}
+        <button
+          className="button"
+          onClick={() => setShowAddFor((showAddFor) => !showAddFor)}
+        >
+          {showAddFor ? 'Закрыть' : 'Добавить друга'}
         </button>
       </article>
 
